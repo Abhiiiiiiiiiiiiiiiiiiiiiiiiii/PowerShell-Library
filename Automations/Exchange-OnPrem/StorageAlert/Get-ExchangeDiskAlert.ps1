fab3e2666cde2a2d4461d87964c6bc94ed6cc702
Abhiiiiiiiiiiiiiiiiiiiiiiiiii <143308207+Abhiiiiiiiiiiiiiiiiiiiiiiiiii@users.noreply.github.com>
2026-06-12 15:21:01 +0530

    Add disk space alert script for Exchange Server
    This script monitors disk space on the specified drive and sends an alert email if free space falls below the defined threshold.

---
+++ Automations/Exchange-OnPrem/StorageAlert/Get-ExchangeDiskAlert.ps1
@@ -0,0 +1,25 @@
+# Define parameters
+$smtpServer = "loadbalancer.Domain.com"
+$from = "Your Sender Email ID"
+$to = "Your Admin Email ID"
+$subject = "Exchange Server Disk Space Alert"
+$thresholdGB = 140  # Set your desired threshold in gigabytes
+$drive = "C:"     # Specify the drive you want to monitor
+# Get disk space information
+$disk = Get-WmiObject -Class Win32_LogicalDisk | Where-Object {$_.DeviceID -eq $drive}
+$freeSpaceGB = [math]::Round($disk.FreeSpace / 1GB, 2)
+# Check if free space is below threshold
+if ($freeSpaceGB -lt $thresholdGB) {
+    $body = "Dear Team,
+This is an automated alert to notify you that the disk usage on your Exchange Server has exceeded the configured threshold.
+Server: Your_Server_Hostname: 
+C drive total space: 255GB
+C drive Free space available C: $freeSpaceGB GB"
+    # Send email
+    $smtp = New-Object Net.Mail.SmtpClient($smtpServer)
+    $msg = New-Object Net.Mail.MailMessage($from, $to, $subject, $body)
+    $smtp.Send($msg)
+}
+else {
+    Write-Output "Disk space is sufficient. No alert sent."
+}
